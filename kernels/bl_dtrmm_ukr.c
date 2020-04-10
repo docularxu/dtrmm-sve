521f59eaa7df5a3e0df9bc3dab837d13ef2bc993
Guodong Xu <guodong.xu@linaro.org>
2020-04-10 21:38:43 +0800

    TRMM_microkernel: optimization loop unrolling, registers
    Add optimization to dtrmm micro kernel. Includes:
    1. unrolling the loop of Br
    2. using register variables for Ar and Cr.
    
    Signed-off-by: Guodong Xu <guodong.xu@linaro.org>

--- kernels/bl_dtrmm_ukr.c
+++ kernels/bl_dtrmm_ukr.c
@@ -19,6 +19,8 @@ void bl_dtrmm_ukr( int    k,
                    int    ya )
 {
     int l, j, i;
+    register double a_il_reg;
+    register double c_ij0_reg, c_ij1_reg, c_ij2_reg, c_ij3_reg;
 
     for ( l = 0; l < k; ++l )                               // Loop 0.1, column [l] of A, times, row [l] of B
     {                 
@@ -29,11 +31,27 @@ void bl_dtrmm_ukr( int    k,
         if ( xa + data->m - 1 < ya + l )
             break;
 
-        for ( j = 0; j < DGEMM_NR; ++j )                    // Loop 0.2, walk through row[l] of B
+        for ( j = 0; j < DGEMM_NR; j+=4 )                    // Loop 0.2, walk through row[l] of B
         { 
             for ( i = 0; i < DGEMM_MR; ++i )                // Loop 0.3, walk through column[l] of A
-            { 
-                c( i, j ) += a( i, l ) * b( l, j );
+            {
+                a_il_reg = a( i, l );
+
+                c_ij0_reg = (double) 0.0;
+                c_ij1_reg = (double) 0.0;
+                c_ij2_reg = (double) 0.0;
+                c_ij3_reg = (double) 0.0;
+
+                c_ij0_reg += a_il_reg * b( l, j + 0 );
+                c_ij1_reg += a_il_reg * b( l, j + 1 );
+                c_ij2_reg += a_il_reg * b( l, j + 2 );
+                c_ij3_reg += a_il_reg * b( l, j + 3 );
+
+                // memory store
+                c( i, j + 0 ) += c_ij0_reg;
+                c( i, j + 1 ) += c_ij1_reg;
+                c( i, j + 2 ) += c_ij2_reg;
+                c( i, j + 3 ) += c_ij3_reg;
             }
         }
     }
